7fab0a82f71b217ad245abd839eb2ae0bb5da9d3
Kamron <kamronpoosti@yahoo.com>
2023-03-13 15:15:16 -0700

    added logo

--- ghi/app/src/MainPage.js
+++ ghi/app/src/MainPage.js
@@ -1,13 +1,16 @@
+import Wardrobify from "./imgs/Wardrobify.png"
+
 function MainPage() {
   return (
     <div className="px-4 py-5 my-5 text-center">
-      <h1 className="display-5 fw-bold">WARDROBIFY!</h1>
-      <div className="col-lg-6 mx-auto">
-        <p className="lead mb-4">
-          Need to keep track of your shoes and hats? We have
-          the solution for you!
-        </p>
-      </div>
+      <div className="px-4 py-5 my-5 text-center">
+        <img
+          src={Wardrobify}
+          className="mx-auto d-block"
+          alt=""
+          style={{ objectFit: "contain", width: "60%", height: "60%" }}
+        />
+    </div>
     </div>
   );
 }
